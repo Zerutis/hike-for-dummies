38b9626fbac8ffacac6ddad81b0d46f076b8c38a
zerutis <rokas.zeruolis@gmail.com>
2023-09-21 14:49:19 +0300

    cors

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,11 +4,21 @@ import io.getquill.SnakeCase
 import io.getquill.jdbczio.Quill
 import zio._
 import zio.http._
-
 import controller.HikeController
 import repo.HikeRepo
 
+import zio.http.Header.{AccessControlAllowOrigin, Origin}
+import zio.http.HttpAppMiddleware.cors
+import zio.http.internal.middlewares.Cors.CorsConfig
+
 object Main extends ZIOAppDefault {
+
+  lazy val corsConfig: CorsConfig = CorsConfig(
+    allowedOrigin = {
+      case origin@Origin.Value(_, host, _) if host == "localhost" => Some(AccessControlAllowOrigin.Specific(origin))
+      case _ => None
+    }
+  )
 
   lazy val postgresLayer = Quill.Postgres.fromNamingStrategy(SnakeCase)
   lazy val dataSourceLayer = Quill.DataSource.fromPrefix("ctx")
@@ -16,7 +26,7 @@ object Main extends ZIOAppDefault {
   lazy val databaseLayer = dataSourceLayer >>> postgresLayer
   lazy val hikeLayer =  databaseLayer >>>  HikeRepo.layer
 
-  lazy val apps = HikeController.app
+  lazy val apps = HikeController.app @@ cors(corsConfig)
   lazy val httpApps = apps.provideLayer(hikeLayer).withDefaultErrorResponse
 
   override def run = for {
